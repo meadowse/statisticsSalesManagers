b7c440ce701c4b2166724babc79bf11af82fd8da
meadowse <meadowse@student.21-school.ru>
2024-10-30 15:47:14 +0300

    finishBack

--- src/statisticsSalesManagersBack/statisticsSalesManagersBack/settings.py
+++ src/statisticsSalesManagersBack/statisticsSalesManagersBack/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashBoard.apps.DashboardConfig',
+    'getKNData.apps.GetkndataConfig',
 ]
 
 MIDDLEWARE = [
--- src/statisticsSalesManagersBack/statisticsSalesManagersBack/urls.py
+++ src/statisticsSalesManagersBack/statisticsSalesManagersBack/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('successManagers', include('dashBoard.urls')),
+    path('getKN', include('getKNData.urls')),
 ]
